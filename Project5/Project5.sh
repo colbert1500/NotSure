cd2da1c126b6d53f5d98e8e67be410ca15fa2277
Colin <ckeehan@me.com>
2018-12-04 10:39:40 -0600

    Update Project5.sh

--- Project5/Project5.sh
+++ Project5/Project5.sh
@@ -4,4 +4,4 @@ sudo curl https://raw.githubusercontent.com/colbert1500/NotSure/master/Project5/
 sudo docker-compose pull
 sudo docker-compose up -d --build
 #sudo docker exec -ti group4_cli sh -c "clear && ./cli.py"
-sudo docker exec -ti group4_cli sh -c "./cli.py"
+sudo docker exec -i group4_cli sh -c "./cli.py"
