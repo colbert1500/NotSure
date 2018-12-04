af9443d05bacdb1c151e885c0aa44f4875a02bba
Colin <ckeehan@me.com>
2018-12-04 10:50:38 -0600

    Update Project5.sh

--- Project5/Project5.sh
+++ Project5/Project5.sh
@@ -3,5 +3,5 @@ cd group4
 sudo curl https://raw.githubusercontent.com/colbert1500/NotSure/master/Project5/docker-compose.yml --output docker-compose.yml
 sudo docker-compose pull
 sudo docker-compose up -d --build
-sudo docker exec -i group4_cli sh -c "clear && ./cli.py"
+sudo docker exec -i group4_cli sh -c "export TERM=xterm && clear && ./cli.py"
 #sudo docker exec -i group4_cli sh -c "./cli.py"
