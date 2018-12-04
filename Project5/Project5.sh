5ee599500c094201be86cb7ba57407f52abe6873
Colin <ckeehan@me.com>
2018-12-04 10:32:50 -0600

    Added automatic deployment shell script

---
+++ Project5/Project5.sh
@@ -0,0 +1,7 @@
+sudo mkdir group4
+cd group4
+sudo curl https://raw.githubusercontent.com/colbert1500/NotSure/master/Project5/docker-compose.yml --output docker-compose.yml
+sudo docker-compose pull
+sudo docker-compose up -d --build
+#sudo docker exec -ti group4_cli sh -c "clear && ./cli.py"
+sudo docker exec -ti group4_cli sh -c "./cli.py"
